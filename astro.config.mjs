aaa6c6da53736be8c69d6078241fe8e66ba900c4
Francisco José Martos Morillo <frank@MacBook-Air-de-Francisco.local>
2026-05-09 15:54:17 +0200

    Fix astro config output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
 });
